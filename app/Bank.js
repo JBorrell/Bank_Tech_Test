2ae44a2755d85b5e5def2465678eac3f928a3d97
JBorrell <boz_012@hotmail.com>
2016-04-07 17:14:47 +0100

    Adds withdraw & deposit functions, adds transactions, transactions show date, type & amount

--- app/Bank.js
+++ app/Bank.js
@@ -1,3 +1,21 @@
-function Bank(){
+function Bank() {
+  this.balance = 0;
+  this.transactions = [];
+};
 
+Bank.prototype.deposit = function (amount) {
+  this.balance += amount
+  this.transactions.push([this._getDate(), 'DEPOSIT', amount])
+};
+
+Bank.prototype.withdraw = function (amount) {
+  this.balance -= amount
+  this.transactions.push([this._getDate(), 'WITHDRAW', amount])
+};
+
+
+Bank.prototype._getDate = function() {
+  var date = new Date().toISOString().substring(0, 10);
+  var format = date.split('-').reverse().join('/')
+  return format
 };
--- test/spec/BankSpec.js
+++ test/spec/BankSpec.js
@@ -6,7 +6,37 @@ describe("Bank", function() {
   });
 
   it("should start with a balance of 0", function() {
-    expect(bank.balance).toEqual(0)
+    expect(bank.balance).toEqual(0);
+  });
+
+  describe("Depositing Money", function(){
+    it("balance should increase by 10", function(){
+      bank.deposit(10);
+      expect(bank.balance).toEqual(10);
+    });
+  });
+
+  describe("Withdrawing Money", function(){
+    beforeEach(function(){
+      bank.deposit(10);
+    });
+    it("balance should decrease by 5", function(){
+      bank.withdraw(5);
+      expect(bank.balance).toEqual(5);
+    });
+  });
+
+  describe("Transaction", function(){
+    it("records date of transaction", function(){
+      bank.deposit(10);
+      expect(bank.transactions[0][0]).toEqual("07/04/2016");
+    });
+    it("shows type of transaction", function(){
+      bank.deposit(4);
+      bank.withdraw(3);
+      expect(bank.transactions[0][1]).toEqual("DEPOSIT");
+      expect(bank.transactions[1][1]).toEqual("WITHDRAW");
+    })
   });
 
 });
